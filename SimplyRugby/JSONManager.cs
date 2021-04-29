d78104ad7703a0c158524f71ff6183166f4746e5
Kamil <kchwedura@gmail.com>
2021-04-29 19:29:56 +0100

    Started the JSON intergration

---
+++ SimplyRugby/JSONManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplyRugby
+{
+    class JSONManager
+    {
+        // Gets the details of the player or coach and serializes it into a JSON string
+        public void ConvertToJSON()
+        {
+
+        }
+
+        // Retrieves the JSON string and deserializes it into an object
+        public void ConvertFromJSON()
+        {
+
+        }
+
+    }
+}
