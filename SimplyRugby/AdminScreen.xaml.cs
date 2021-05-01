c33e05351af425bcbcafbefc0b89fc8f79d4e041
Kamil Chwedura <kchwedura@gmail.com>
2021-05-01 22:30:49 +0100

    Added Field Checks for Coach and Player fields

--- SimplyRugby/AdminScreen.xaml.cs
+++ SimplyRugby/AdminScreen.xaml.cs
@@ -49,67 +49,83 @@ namespace SimplyRugby
         //Adding Player Details to a JSON File
         private void btnAddPlayer_Click(object sender, RoutedEventArgs e)
         {
-            string squad;
-            int age = Int32.Parse(txtPlayerAge.Text);
-            Player player = new Player();
-
-            #region Squad Age Check
-            // Automatically checks which Squad the Player should be in depending on their age
-            if (age <= 14)
+            // Checks if any fields are empty
+            if (txtPlayerName.Text == null || txtPlayerAge.Text == null)
             {
-                squad = "Under 15s";
-            }
-            else if (age == 15)
-            {
-                squad = "Under 16s";  
-            }
-            else if (age == 16 || age == 17)
-            {
-                squad = "Under 18s";
-            }
-            else if (age == 18 || age == 19)
-            {
-                squad = "Under 20s";
+                MessageBox.Show("Please make sure ALL of the Player fields are filled in!");
             }
             else
             {
-                squad = "Seniors";
+                string squad;
+                int age = Int32.Parse(txtPlayerAge.Text);
+                Player player = new Player();
+
+                #region Squad Age Check
+                // Automatically checks which Squad the Player should be in depending on their age
+                if (age <= 14)
+                {
+                    squad = "Under 15s";
+                }
+                else if (age == 15)
+                {
+                    squad = "Under 16s";
+                }
+                else if (age == 16 || age == 17)
+                {
+                    squad = "Under 18s";
+                }
+                else if (age == 18 || age == 19)
+                {
+                    squad = "Under 20s";
+                }
+                else
+                {
+                    squad = "Seniors";
+                }
+                #endregion
+
+                #region Creating a Player Object
+                // Package the Player Details into an Object
+                player.name = txtPlayerName.Text;
+                player.age = age;
+                player.squad = squad;
+                // The 0s are because Admins are not allowed to change or add Skill data, the coach can change these values
+                player.running = 0;
+                player.tackling = 0;
+                player.passing = 0;
+                player.throwing = 0;
+                player.comments = "";
+                #endregion
+
+                json.ConvertToJSON("Players.json", player, null);
+
+                // I make a formatted string with all of the information pulled from the Player Class and display it to the user
+                string formattedString = string.Format("Successfully added a Player!\n\nPlayer Details:\nName: {0}\nAge: {1}\nSquad: {2}", player.name, player.age, player.squad);
+                MessageBox.Show(formattedString);
             }
-            #endregion
-
-            #region Creating a Player Object
-            // Package the Player Details into an Object
-            player.name = txtPlayerName.Text;
-            player.age = age;
-            player.squad = squad;
-            // The 0s are because Admins are not allowed to change or add Skill data, the coach can change these values
-            player.running = 0;
-            player.tackling = 0;
-            player.passing = 0;
-            player.throwing = 0;
-            player.comments = "";
-            #endregion
-
-            json.ConvertToJSON("Players.json", player, null);
-
-            // I make a formatted string with all of the information pulled from the Player Class and display it to the user
-            string formattedString = string.Format("Successfully added a Player!\n\nPlayer Details:\nName: {0}\nAge: {1}\nSquad: {2}", player.name, player.age, player.squad);
-            MessageBox.Show(formattedString);
         }
 
         private void btnAddCoach_Click(object sender, RoutedEventArgs e)
         {
-            Coach coach = new Coach();
+            // Checks if any fields are empty
+            if(txtCoachName.Text == null || txtCoachEmail.Text == null)
+            {
+                MessageBox.Show("Make sure ALL of the Coach fields are filled in!");
+            }
+            else
+            {
+                Coach coach = new Coach();
 
-            // Package the Coach Details into an Object
-            coach.name = txtCoachName.Text;
-            coach.email = txtCoachEmail.Text;
+                // Package the Coach Details into an Object
+                coach.name = txtCoachName.Text;
+                coach.email = txtCoachEmail.Text;
 
-            json.ConvertToJSON("Coaches.json", null, coach);
+                json.ConvertToJSON("Coaches.json", null, coach);
 
-            // I make a formatted string with all of the information pulled from the Coach Class and display it to the user
-            string formattedString = string.Format("Successfully added a Coach!\n\nCoach Details:\nName: {0}\nE-Mail: {1}", coach.name, coach.email);
-            MessageBox.Show(formattedString);
+                // I make a formatted string with all of the information pulled from the Coach Class and display it to the user
+                string formattedString = string.Format("Successfully added a Coach!\n\nCoach Details:\nName: {0}\nE-Mail: {1}", coach.name, coach.email);
+                MessageBox.Show(formattedString);
+            }
         }
     }
 }
